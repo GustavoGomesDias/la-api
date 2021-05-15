0b2586a4f13030e839f100e9e25874ba19cc540e
GustavoGomesDias <treeelevengg@gmail.com>
2021-05-15 02:19:40 -0300

    feat: middleware de authetication

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import userController from '../controllers/UserController';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = Router();
 
 // create = store
 
 router.post('/', userController.store);
-router.get('/', userController.index);
+router.get('/', loginRequired, userController.index);
 router.get('/:id', userController.show);
-router.put('/:id', userController.update);
-router.delete('/:id', userController.delete);
+router.put('/:id', loginRequired, userController.update);
+router.delete('/:id', loginRequired, userController.delete);
 
 export default router;
