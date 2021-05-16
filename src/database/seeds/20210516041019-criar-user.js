dd1916741d9d9d865fc88bd7a5709532e3080c47
GustavoGomesDias <treeelevengg@gmail.com>
2021-05-16 01:28:01 -0300

    chore: add data in user seeders

--- src/database/seeds/20210516041019-criar-user.js
+++ src/database/seeds/20210516041019-criar-user.js
@@ -1,24 +1,24 @@
-'use strict';
+const bcrypt = require('bcrypt');
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface) => {
+    await queryInterface.bulkInsert('users', [
+      {
+        nome: 'John Doe',
+        email: 'jhon@email.com',
+        password_hash: bcrypt.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'John Doe 2',
+        email: 'jhon2@email.com',
+        password_hash: bcrypt.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ], {});
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  down: async () => {},
 };
