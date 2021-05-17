d224144ac29f08bc370697799cece01a9675dc97
GustavoGomesDias <treeelevengg@gmail.com>
2021-05-16 23:02:57 -0300

    chore: update files

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const aluno = await Aluno.create({
-      nome: 'Teste',
-      sobrenome: 'TesteSobre',
-      email: 'email@email.com',
-      idade: 12,
-      peso: 14,
-      altura: 2.5,
-    });
-    res.json(aluno);
+    res.json({ index: 'Index' });
   }
 }
 
