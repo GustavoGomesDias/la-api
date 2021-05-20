41fa402909f3c8e963f46a08836e210481b45aba
GustavoGomesDias <treeelevengg@gmail.com>
2021-05-19 23:36:17 -0300

    feat: add images migration

--- src/routes/imgRoutes.js
+++ src/routes/imgRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import imgController from '../controllers/ImgController';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = Router();
 
-router.post('/', imgController.store);
+router.post('/', loginRequired, imgController.store);
 
 export default router;
