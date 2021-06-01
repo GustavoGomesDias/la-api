f0f9abf91c946df41a1ac8ef7ed8ee9250092387
GustavoGomesDias <treeelevengg@gmail.com>
2021-05-31 23:55:00 -0300

    feat: add cors

--- app.js
+++ app.js
@@ -4,12 +4,29 @@ dotenv.config();
 
 import './src/database';
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
 import { resolve } from 'path';
 import homeRoutes from './src/routes/homeRoutes';
 import userRoutes from './src/routes/userRoutes';
 import tokenRoutes from './src/routes/tokenRoutes';
 import alunoRoutes from './src/routes/alunoRoutes';
 import imgRoutes from './src/routes/imgRoutes';
+
+const whiteList = [
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -19,6 +36,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
